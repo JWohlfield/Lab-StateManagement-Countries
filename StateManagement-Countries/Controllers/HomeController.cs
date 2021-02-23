7c003921a230827657aefcf386d4ee92c18cc26a
Jeffrey <JWohlfield@hotmail.com>
2021-02-22 21:50:34 -0500

    Initial commit

---
+++ StateManagement-Countries/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StateManagement_Countries.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagement_Countries.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Country> countries = new List<Country>
+            {
+                new Country{Name = "USA", Language = "English", Greeting = "Hello world!", Description = "United States", Colors = new [] {"red" , "white", "blue"} },
+                new Country{Name = "MEX", Language = "Spanish", Greeting = "Hola mundo!", Description = "Mexico", Colors = new [] {"red" , "white", "greeen"} }
+
+            };
+            return View(countries);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Country c)
+        {
+            TempData["Name"] = c.Name;
+            TempData["Description"] = c.Description;
+
+            return RedirectToAction("Details", c);
+
+        }
+
+        public IActionResult Details(Country c)
+        {
+            return View(c);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StateManagement-Countries/Models/Country.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagement_Countries.Models
+{
+    public class Country
+    {
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string Greeting { get; set; }
+        public string Description { get; set; }
+        public string[] Colors { get; set; }
+
+    }
+}
